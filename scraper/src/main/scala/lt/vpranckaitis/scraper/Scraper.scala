f50d0e50a3754cee4f7f7031f7da90493f0da5e5
Vilius Pranckaitis <vpranckaitis@gmail.com>
2016-03-20 13:36:48 +0200

    Making 15min.lt scraper more robust

--- scraper/src/main/scala/lt/vpranckaitis/scraper/Scraper.scala
+++ scraper/src/main/scala/lt/vpranckaitis/scraper/Scraper.scala
@@ -3,6 +3,7 @@ package lt.vpranckaitis.scraper
 import com.typesafe.scalalogging.StrictLogging
 import lt.vpranckaitis.documentclustering.storage.Storage
 import net.ruippeixotog.scalascraper.browser.Browser
+import org.jsoup.Connection
 
 import scala.concurrent.Await
 import scala.concurrent.duration._
@@ -10,9 +11,17 @@ import scala.concurrent.duration._
 object Scraper extends App with StrictLogging {
 
   val storage = new Storage()
-  val browser = new Browser()
+  val browser = new Browser {
+    override def requestSettings(conn: Connection) =
+      conn
+        .userAgent("Mozilla/5.0 (Windows; U; WindowsNT 5.1; en-US; rv1.8.1.6) Gecko/20070725 Firefox/2.0.0.6")
+        .referrer("http://www.google.com")
+        .header("Accept", "text/html,application/xhtml+xml,application/xml")
+        .header("Accept-Charset", "utf-8")
+        .timeout(15000)
+  }
 
-  val future = new Z15minScraper(storage, browser).scrape("http://www.15min.lt/ajax/articles/list?offset=2016-03-15 12:32:41")
+  val future = new Z15minScraper(storage, browser).scrape("http://www.15min.lt/ajax/articles/list?offset=2016-02-01 16:57:17")
 
   logger.info(Await.result(future, Duration.Inf).toString)
 }
--- scraper/src/main/scala/lt/vpranckaitis/scraper/Z15minScraper.scala
+++ scraper/src/main/scala/lt/vpranckaitis/scraper/Z15minScraper.scala
@@ -7,12 +7,25 @@ import net.ruippeixotog.scalascraper.browser.Browser
 import net.ruippeixotog.scalascraper.dsl.DSL.Extract._
 import net.ruippeixotog.scalascraper.dsl.DSL._
 import org.joda.time.DateTime
+import org.jsoup.Connection
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 import scala.util.{Failure, Success, Try}
 
-class Z15minScraper(storage: Storage, browser: Browser) extends StrictLogging {
+class Z15minScraper(storage: Storage, var browser: Browser) extends StrictLogging {
+  def reconnectBrowser(): Unit = {
+    browser = new Browser {
+      override def requestSettings(conn: Connection) =
+        conn
+          .userAgent(s"Mozilla/5.0 (Windows; U; WindowsNT 5.1; en-US; rv1.8.1.6) Gecko/20070725 Firefox/2.0.0.${System.currentTimeMillis}")
+          .referrer("http://www.google.com")
+          .header("Accept", "text/html,application/xhtml+xml,application/xml")
+          .header("Accept-Charset", "utf-8")
+          .timeout(15000)
+    }
+  }
+
   private def scrapeAndSave(url: String): Future[Int] = {
     val scrape = Try {
       val doc = browser.get(url)
@@ -36,8 +49,6 @@ class Z15minScraper(storage: Storage, browser: Browser) extends StrictLogging {
       val description: String = metas >> text("*[itemprop=description]")
       val articleText: String = articleContent >> text("*")
 
-      Thread.sleep(1000)
-
       Article(source, url, category, subcategory, date, title, description, articleText)
     }
 
@@ -49,25 +60,34 @@ class Z15minScraper(storage: Storage, browser: Browser) extends StrictLogging {
             0
         }
       case Failure(ex) =>
-        logger.error("Failed scraping", ex)
-        Future.successful(0)
+        logger.error(s"Failed scraping: $url", ex)
+        Thread.sleep(5000)
+        reconnectBrowser()
+        scrapeAndSave(url)
     }
   }
 
   def scrape(url: String): Future[Int] = {
     def iterate(url: String): Stream[String] = {
-      val document = browser.get(url)
+      val document = Try(browser.get(url)) match {
+        case Success(x) => x
+        case _ =>
+          logger.warn("Failed to get the article list")
+          Thread.sleep(5000)
+          reconnectBrowser()
+          browser.get(url)
+      }
 
       val articles = document.select(".wrapper")
       articles.select(".widget").remove()
       val articleUrls = articles >> elementList(".item-content h3 a") >> attr("href")("a")
       val nextOffset = "offset: '(\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2})'".r.findFirstMatchIn((document >> elements("script")).toString).fold("0"){x => x.group(1)}
-      //val nextPage =
+      println(nextOffset)
       val nextUrl = s"http://www.15min.lt/ajax/articles/list?offset=$nextOffset"
       articleUrls.toStream #::: iterate(nextUrl)
     }
 
-    val futures = iterate(url) take 30000 map scrapeAndSave
+    val futures = iterate(url) take 300000 map scrapeAndSave
     Future.fold(futures)(0)(_ + _)
   }
 }
